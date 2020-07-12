acc0b10593f7b10398722ca8a78c35a28b4bacab
Priyanka Nagar <priyanka.nagar@nordstrom.com>
2020-07-11 21:36:09 -0700

    added a POST method test for OrderController

---
+++ src/test/java/com/example/orders/OrderControllerTests.java
@@ -0,0 +1,71 @@
+package com.example.orders;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class OrderControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PurchaseOrderService orderService;
+
+    @MockBean
+    private JwtAuthenticationController jwtAuthenticationController;
+
+    @MockBean
+    private JwtRequestFilter jwtRequestFilter;
+
+    @MockBean
+    private JwtUserDetailsService jwtUserDetailsService;
+
+    @MockBean
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    @Disabled
+    @Test
+    public void testPostExample() throws Exception {
+
+        OrderDetails orderDetails1 =
+                new OrderDetails("Elsa Costume" + UUID.randomUUID().toString(), new BigDecimal(12));
+
+        OrderDetails orderDetails2 = new OrderDetails("Anna Costume" + UUID.randomUUID().toString(),
+                new BigDecimal(15));
+
+        List<OrderDetails> orderDetailsList = List.of(orderDetails1, orderDetails2);
+
+        PurchaseOrder order = new PurchaseOrder(new BigDecimal(1), orderDetailsList);
+        Mockito.when(orderService.createOrder(ArgumentMatchers.any())).thenReturn(order);
+        String json = mapper.writeValueAsString(order);
+
+        mockMvc.perform(post("/ordrwereowriers").header("Authorization", "Bearer 5u43959").
+                contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8")
+                .content(json).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.orderAmount", Matchers.equalTo(1)));
+
+
+    }
+}
--- src/test/java/com/example/orders/OrderRepositoryTests.java
+++ src/test/java/com/example/orders/OrderRepositoryTests.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -27,12 +26,11 @@ public class OrderRepositoryTests {
 
         OrderDetails orderDetails1 =
                 new OrderDetails("Elsa Costume" + UUID.randomUUID().toString(), new BigDecimal(12));
-        List<OrderDetails> orderDetailsList = new ArrayList<>();
-        orderDetailsList.add(orderDetails1);
-
+        
         OrderDetails orderDetails2 = new OrderDetails("Anna Costume" + UUID.randomUUID().toString(),
                 new BigDecimal(15));
-        orderDetailsList.add(orderDetails2);
+
+        List<OrderDetails> orderDetailsList = List.of(orderDetails1, orderDetails2);
 
         PurchaseOrder purchaseOrder = new PurchaseOrder(new BigDecimal(12.34), orderDetailsList);
 
